002108b0699d9f3be9ba02a45ca3462c3f320853
Alakdae <alakdae@gmail.com>
2025-04-04 17:40:18 +0200

    Add description

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="aquastilla_softener",
     description="Library to fetch data from Aquastilla softener from Viessmann API",
-    version="0.1.2",
+    version="0.1.3",
     license="GPL",
     author="Tomasz Szymanowicz",
     author_email="alakdae@gmail.com",
