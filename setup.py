d97fe5a2be2c0d41d59942955db6c39b8821b43f
Alakdae <alakdae@gmail.com>
2025-04-05 19:07:46 +0200

    Add proper README

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aquastilla_softener",
     description="Library to fetch data from Aquastilla softener from Viessmann API",
+    long_description=long_description,
     version="0.1.3",
     license="GPL",
     author="Tomasz Szymanowicz",
