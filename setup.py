4dfd0f1d25b3d20670680ba2dac8fe6ff5a074c5
Alakdae <alakdae@gmail.com>
2025-04-05 19:11:09 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="aquastilla_softener",
     description="Library to fetch data from Aquastilla softener from Viessmann API",
     long_description=long_description,
-    version="0.1.4",
+    version="0.1.5",
     license="GPL",
     author="Tomasz Szymanowicz",
     author_email="alakdae@gmail.com",
