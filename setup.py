6f5d285d34168ef386f39f9c6ac93a8a9b4c231f
Alakdae <alakdae@gmail.com>
2025-04-05 19:15:55 +0200

    Add README

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-long_description = ("README.md").read_text(encoding="utf-8")
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="aquastilla_softener",
     description="Library to fetch data from Aquastilla softener from Viessmann API",
     long_description=long_description,
-    version="0.1.6",
+    long_description_content_type="text/markdown",
+    version="0.1.7",
     license="GPL",
     author="Tomasz Szymanowicz",
     author_email="alakdae@gmail.com",
